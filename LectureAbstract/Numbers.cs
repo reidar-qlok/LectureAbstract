6b753b8cb6e5d42310afe05b912a01bf11131113
Reidar Nilsen <reidar.nilsen@qlok.se>
2022-10-24 15:51:15 +0200

    Add project files.

---
+++ LectureAbstract/Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LectureAbstract
+{
+    abstract class Numbers
+    {
+        protected int nr1 = 100;
+        protected int nr2 = 150;
+        public abstract void NumberMethod();
+
+        public abstract int First { get; }
+        public abstract int Second { get; }
+    }
+}
---
+++ LectureAbstract/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LectureAbstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new DerivedClass();
+            obj.NumberMethod();
+            Console.WriteLine($"nr1 = {obj.First}, nr2 = {obj.Second}");
+        }
+    }
+    class DerivedClass : Numbers
+    {
+        public override int First
+        {
+            get
+            {
+                return nr1 + 4;
+            }
+        }
+        public override int Second
+        {
+            get
+            {
+                return nr2 + 2;
+            }
+        }
+        public override void NumberMethod()
+        {
+            nr1++;
+            nr2++;
+        }
+    }
+}
